ca01c93f709722f43afda22e7820c988ad6cc6a1
SarahGoli92 <sarah.alalawi92@gmail.com>
2024-05-06 12:14:55 +0300

    defined JS variables

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-let flip = false
-const reStart = document.getElementById('resBtn')
-let cardsToggled = []
-
 let myCards = document.querySelectorAll('.card')
+let flippedCards = []
+const reStart = document.getElementById('resBtn')
 
-console.log(myCards)
+const frontImages = [
+  {
+    id: 1,
+    image: 'images/1.jpg'
+  },
+  {
+    id: 2,
+    image: 'images/2.jpg'
+  },
+  {
+    id: 3,
+    image: 'images/5.jpg'
+  },
+  { id: 4, image: 'images/1.jpg' },
+  { id: 5, image: 'images/2.jpg' },
+  {
+    id: 6,
+    image: 'images/5.jpg'
+  }
+]
+
+const gameRestart = () => {
+  let flippedCard = document.getElementsByClassName('card front')
+  frontImages.sort(() => Math.random() - 0.5)
+}
+reStart.addEventListener('click', gameRestart())
